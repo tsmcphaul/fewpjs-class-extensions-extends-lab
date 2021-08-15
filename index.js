95792597c57045fa3142d6b3a53bce059635cbd5
Tammarra McPhaul <shaemc22@gmail.com>
2021-08-15 19:14:49 -0400

    commit tree 3773f446873f18801e7159e9228034819f11d7d4

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((s, sum) => s + sum);
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        return (this.sides[0] + this.sides[1] > this.sides[2]) && 
+        (this.sides[1] + this.sides[2] > this.sides[0]) && 
+        (this.sides[0] + this.sides[2] > this.sides[1])
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.sides[0] === this.sides[1] &&
+        this.sides[0] === this.sides[2] &&
+        this.sides[0] === this.sides[3]
+    }
+
+    get area() {
+        return this.sides[0] ** 2
+    }
+}
